390538ab6f297a0997af1313a8f0bacfa9741f13
Dyego Soriano <dyegodf2@gmail.com>
2020-02-10 13:05:04 -0300

    configuração inicial do servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Olá mundo!')
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  return res.json({ message: 'Olá mundo!' })
+})
+
+server.listen(3000)
