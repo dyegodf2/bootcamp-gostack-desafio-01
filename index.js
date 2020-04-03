cbd06eaf96d8499595110e3ff4b3317072b6ef1d
Dyego Soriano <dyegodf2@gmail.com>
2020-04-02 22:57:03 -0300

    separação do array

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const express = require('express')
+const projects = require('./projects')
 
 const server = express()
 
 let request = 0
-const projects = [
-  {
-    id: '0',
-    title: 'Projeto 0',
-    tasks: []
-  },
-  {
-    id: '1',
-    title: 'Projeto 1',
-    tasks: []
-  },
-  {
-    id: '3',
-    title: 'Projeto 3',
-    tasks: []
-  }
-]
 
 // Middlewares
 server.use(countRequest, express.json())
---
+++ projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    id: '0',
+    title: 'Projeto 0',
+    tasks: []
+  },
+  {
+    id: '1',
+    title: 'Projeto 1',
+    tasks: []
+  },
+  {
+    id: '3',
+    title: 'Projeto 3',
+    tasks: []
+  }
+]
+
+module.exports = projects
